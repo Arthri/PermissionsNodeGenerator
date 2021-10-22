a78c73f7126cbfe667148dc22c6a79a366559c5d
Arthri <41360489+Arthri@users.noreply.github.com>
2021-10-22 23:41:28 +0800

    feat: Add permission node

---
+++ src/PermissionsNodeGenerator/PermissionNode.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace PermissionsNodeGenerator
+{
+    /// <summary>
+    /// Represents a node in a permission tree.
+    /// </summary>
+    public readonly struct PermissionNode
+    {
+        /// <summary>
+        /// Gets the parent of this node.
+        /// </summary>
+        public PermissionNode? Parent { get; }
+
+        /// <summary>
+        /// Gets the name of this node.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Gets the children of this node.
+        /// </summary>
+        public List<PermissionNode> Children { get; }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="PermissionNode"/>.
+        /// </summary>
+        /// <param name="name">The name of the node.</param>
+        /// <param name="parent">The parent of the node.</param>
+        internal PermissionNode(
+            string name,
+            PermissionNode? parent = null)
+        {
+            Name = name;
+            Parent = parent;
+            Children = new List<PermissionNode>();
+        }
+    }
+}

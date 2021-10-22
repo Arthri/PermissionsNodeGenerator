7b5a452f5c69389755f0ed87d1d897de217d4d70
Arthri <41360489+Arthri@users.noreply.github.com>
2021-10-22 23:36:11 +0800

    feat: Add basic generator

---
+++ src/PermissionsNodeGenerator/PermissionsNodeSourceGenerator.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace PermissionsNodeGenerator
+{
+    /// <summary>
+    /// Represents a source code generator for Permission Nodes.
+    /// </summary>
+    [Generator]
+    public class PermissionsNodeSourceGenerator : ISourceGenerator
+    {
+        /// <inheritdoc />
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+
+        /// <inheritdoc />
+        public void Execute(GeneratorExecutionContext context)
+        {
+        }
+    }
+}

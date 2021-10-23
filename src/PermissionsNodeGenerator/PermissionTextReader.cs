1cc2876ec2df259e54c9a1b2b3e17d09dbca48e1
Arthri <41360489+Arthri@users.noreply.github.com>
2021-10-23 15:11:24 +0800

    feat: method to determine if name is valid

--- src/PermissionsNodeGenerator/PermissionTextReader.cs
+++ src/PermissionsNodeGenerator/PermissionTextReader.cs
@@ -5,5 +5,26 @@
     /// </summary>
     public static class PermissionTextReader
     {
+        /// <summary>
+        /// Determines if the specified name is valid.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns><see langword="true"/> if the name is valid, otherwise <see langword="false"/>.</returns>
+        public static bool IsValidName(string name)
+        {
+            for (int i = 0; i < name.Length; i++)
+            {
+                char c = name[i];
+
+                if (c == '.')
+                {
+                    // Once we find an undesirable character, short circuit
+                    // and return false
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }

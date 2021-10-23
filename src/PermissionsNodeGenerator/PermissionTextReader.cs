856ab26da22b7941483b22dea1eab8c650a96abd
Arthri <41360489+Arthri@users.noreply.github.com>
2021-10-23 18:03:44 +0800

    feat: Add basic parsing

--- src/PermissionsNodeGenerator/PermissionTextReader.cs
+++ src/PermissionsNodeGenerator/PermissionTextReader.cs
@@ -1,4 +1,7 @@
-﻿namespace PermissionsNodeGenerator
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace PermissionsNodeGenerator
 {
     /// <summary>
     /// Provides methods to read a permission text file.
@@ -31,7 +34,7 @@
         /// Determines the indent count of the specified line.
         /// </summary>
         /// <param name="line">The line.</param>
-        /// <returns>The number of indent.</returns>
+        /// <returns>The indent count.</returns>
         public static int CountIndent(string line)
         {
             // The indent count
@@ -46,6 +49,78 @@
             }
 
             return i;
+        }
+
+        /// <summary>
+        /// Parses the specified stream as a permissions document.
+        /// </summary>
+        /// <param name="stream">The stream.</param>
+        /// <returns>An array of permissions nodes representing the parsed document.</returns>
+        public static IReadOnlyList<PermissionNode> Parse(Stream stream)
+        {
+            var rootNode = new PermissionNode("", null);
+
+            using (var reader = new StreamReader(stream))
+            {
+                // A stack representing the hierachy of nodes
+                var nodeStack = new Stack<PermissionNode>();
+                nodeStack.Push(rootNode);
+
+                // The depth, also known as the indent to read for
+                var depth = 0;
+
+                // The last node parsed
+                PermissionNode previousNode = null;
+
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var indent = CountIndent(line);
+
+                    if (indent < depth)
+                    {
+                        do
+                        {
+                            nodeStack.Pop();
+                        }
+                        while (indent < --depth);
+                    }
+                    else if (indent == depth + 1)
+                    {
+                        if (previousNode is null)
+                        {
+                            throw null;
+                            // invalid format
+                        }
+
+                        nodeStack.Push(previousNode);
+                        depth++;
+                    }
+                    else if (indent != depth)
+                    {
+                        throw null;
+                        // invalid format
+                    }
+
+                    string name = line.Trim();
+
+                    if (!IsValidName(name))
+                    {
+                        throw null;
+                        // todo: invalid name
+                    }
+
+                    var parent = nodeStack.Peek();
+                    var currentNode = new PermissionNode(name, parent);
+                    parent.Children.Add(currentNode);
+
+                    previousNode = currentNode;
+                }
+            }
+
+
+
+            return rootNode.Children.AsReadOnly();
         }
     }
 }

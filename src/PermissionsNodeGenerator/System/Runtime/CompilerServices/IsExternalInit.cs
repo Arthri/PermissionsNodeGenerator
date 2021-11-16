5d7807a944b30e4ace5250fb156adb4fa62bc6b2
Arthri <41360489+Arthri@users.noreply.github.com>
2021-11-16 18:10:33 +0800

    build: Make init-only setters work

---
+++ src/PermissionsNodeGenerator/System/Runtime/CompilerServices/IsExternalInit.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// This class exists for init-only setters to work.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal static class IsExternalInit
+    {
+    }
+}

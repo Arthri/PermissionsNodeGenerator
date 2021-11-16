6f5d526ba882057ea7f7ab06e3d8755c38b54587
Arthri <41360489+Arthri@users.noreply.github.com>
2021-11-16 17:41:13 +0800

    feat: Add AnalyzerConfigOptions extensions

---
+++ src/PermissionsNodeGenerator/Extensions/AnalyzerConfigOptionsExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace PermissionsNodeGenerator.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="AnalyzerConfigOptions"/>.
+    /// </summary>
+    public static class AnalyzerConfigOptionsExtensions
+    {
+        /// <summary>
+        /// Returns the value of an MSBuild item metadata with the specified name.
+        /// </summary>
+        /// <param name="analyzerConfigOptions">The analyzer options.</param>
+        /// <param name="metadataName">The metadata name.</param>
+        /// <returns>The value of the specified MSBuild item metadata.</returns>
+        public static string GetMetadata(this AnalyzerConfigOptions analyzerConfigOptions, string metadataName)
+        {
+            analyzerConfigOptions
+                .TryGetValue(
+                    $"build_metadata.AdditionalFiles.{metadataName}",
+                    out var result);
+
+            return result;
+        }
+    }
+}
---
+++ src/PermissionsNodeGenerator/Extensions/AnalyzerConfigOptionsProviderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace PermissionsNodeGenerator.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="AnalyzerConfigOptionsProvider"/>.
+    /// </summary>
+    public static class AnalyzerConfigOptionsProviderExtensions
+    {
+        /// <summary>
+        /// Returns the value of an MSBuild property with the specified name.
+        /// </summary>
+        /// <param name="analyzerConfigOptions">The analyzer options.</param>
+        /// <param name="propertyName">The property name.</param>
+        /// <returns>The value of the specified MSBuild property.</returns>
+        public static string GetProperty(this AnalyzerConfigOptionsProvider analyzerConfigOptions, string propertyName)
+        {
+            analyzerConfigOptions
+                .GlobalOptions
+                .TryGetValue(
+                    $"build_property.{propertyName}",
+                    out var result);
+
+            return result;
+        }
+    }
+}

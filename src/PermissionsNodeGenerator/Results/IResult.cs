4972a1f122e590e768ce9caccb69fe3ef0f0111d
Arthri <41360489+Arthri@users.noreply.github.com>
2021-11-21 00:07:46 +0800

    feat: IResult interface

---
+++ src/PermissionsNodeGenerator/Results/IResult.cs
@@ -0,0 +1,34 @@
+﻿namespace PermissionsNodeGenerator.Results
+{
+    /// <summary>
+    /// Represents the result of an operation.
+    /// </summary>
+    public interface IResult
+    {
+        /// <summary>
+        /// The result of the operation.
+        /// </summary>
+        /// <remarks>The value depends on the implementation. It can be null, partial, or full.</remarks>
+        object? Value { get; }
+
+        /// <summary>
+        /// Gets a value indicating if the operation succeeded.
+        /// </summary>
+        bool Success { get; }
+
+        /// <summary>
+        /// Gets a value describing the result.
+        /// </summary>
+        string Message { get; }
+    }
+
+    /// <summary>
+    /// The generic interface for <see cref="IResult"/>.
+    /// </summary>
+    /// <typeparam name="T">The result type.</typeparam>
+    public interface IResult<T> : IResult
+    {
+        /// <inheritdoc />
+        new T? Value { get; }
+    }
+}
